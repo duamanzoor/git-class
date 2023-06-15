19c4aaef8adfcbc61d220ceea6cbb306af7b2f49
duamanzoor <duamanzoor8@gmail.com>
2023-06-15 16:19:03 -0700

    first commit

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main (){
+    print("Hello World");
+
+    var email = duamanzoor8@gmail.com;
+    var password = 1234;
+
+    if (email == email && password == password){
+        print("login successfull");
+        }
+        if(email != email || password != password){
+            print("login unsuccessfull");
+        }
+        
+}
+
